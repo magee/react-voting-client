b4ca7d53c5b228c28aed4a669661083b308e0624
Magee Mooney <mageemooney@me.com>
2016-05-31 21:56:20 -0700

    (feat) adds initial onClick behavior to voting buttons

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import Voting from './components/Voting';
 
 const pair = ['Trainspotting', '28 Days Later'];
+const hasVoted = '';
 
 ReactDOM.render(
-  <Voting pair={pair} />,
+  <Voting pair={pair} hasVoted={hasVoted} />,
   document.getElementById('app')
 );
