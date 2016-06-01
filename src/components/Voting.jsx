402694fb1aea8bd77c585138bcbf1a31ddcb575e
Magee Mooney <mageemooney@me.com>
2016-05-31 18:47:45 -0700

    (debug) public path problem with rendering component?

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -5,12 +5,12 @@ export default React.createClass({
     return this.props.pair || [];
   },
   render: function() {
-    return (<div><h1>Testing</h1><div className="voting">
-          {this.getPair().map(entry =>
-            <button key={entry}>
-              <h1>{entry}</h1>
-            </button>
-          )}
-        </div></div>);
+    return (<div className="voting">
+      {this.getPair().map(entry =>
+        <button key={entry}>
+          <h1>{entry}</h1>
+        </button>
+      )}
+    </div>);
   }
 });
